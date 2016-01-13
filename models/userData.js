857f8caa85261d51316422b0f1a78c478b217960
madcatmk2 <hason.ng@gmail.com>
2016-01-13 20:33:38 +0800

    Add user data files

---
+++ models/userData.js
@@ -0,0 +1,33 @@
+'use strict';
+
+// load the things we need
+var mongoose = require('mongoose');
+
+// define the schema for our user  datamodel
+var userDataSchema = mongoose.Schema({
+
+  username       : String,
+  personalInfo   : {
+//     username     : String,
+    givenName    : String,
+    middleName   : String,
+    familyName   : String,
+    mobileNumber : Number
+  },
+  shippingInfo   : {
+//     username     : String,
+    fullName     : String,
+    phoneNumber  : Number,
+    address1     : String,
+    address2     : String,
+    city         : String,
+    county       : String,
+    postalCode   : String,
+    country      : String
+  }
+
+});
+
+// methods ======================
+// create the model for user data and expose it to our app
+module.exports = mongoose.model('UserData', userDataSchema);
