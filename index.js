c35f424e629baadb589df516bedc1d93d3ebeb21
Herman Ng <heymanhn@gmail.com>
2016-05-11 15:50:09 -0700

    Set CORS header to allow frontend to query API server

--- index.js
+++ index.js
@@ -37,6 +37,14 @@ app.use('/api', kraken(options));
   }
 }); */
 
+app.use(function(req, res, next) {
+    // Set permissive CORS header - this allows this server to be used only as
+    // an API server
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    next();
+});
+
 app.on('start', function () {
     console.log('Application ready to serve requests.');
     console.log('Environment: %s', app.kraken.get('env:env'));
