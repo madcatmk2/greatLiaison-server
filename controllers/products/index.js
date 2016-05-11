8e12017590b7e9bc1997a499a28f0c54e07647f0
Herman Ng <heymanhn@gmail.com>
2016-05-11 02:38:35 -0700

    Implement and RESTify products API routes
    This includes GET, POST, and DELETE operations for products.
    [Finishes #119339683]

--- controllers/products/index.js
+++ controllers/products/index.js
@@ -1,130 +1,147 @@
-/**
-   * A very simple product editor
-   */
 'use strict';
 
-//var bignum = require('bignum');
-var BigDecimal = require('bigdecimal');
-
+var _ = require('underscore');
 var Product = require('../../models/productModel');
 
-module.exports = function (app) {
+module.exports = function(app) {
 
+  /**
+   * @api {get} /products Get products
+   * @apiGroup products
+   *
+   * @apiDescription Retrieves a list of all the G&L products.
+   * @apiSuccess {Object} products List of products.
+   */
   app.get('/', function (req, res) {
-    res.json({ message: 'Product manipulation' });
+    Product.find(function (err, products) {
+      if (err) {
+        console.log("GET /products error: " + err);
+        return res.status(500).send('Server error');
+      }
+
+      res.json({
+        success: true,
+        products: products
+      });
+    });
   });
 
   /**
-     *  Retrieve a list of all products for editing.
-     */
-  app.get('/all', function (req, res) {
+   * @api {get} /products/:productId Get product
+   * @apiGroup products
+   *
+   * @apiParam {String} productId  Mongo product ID.
+   *
+   * @apiDescription Retrieves details about one product
+   * @apiSuccess {Object} product Product details.
+   */
+  app.get('/:productId', function (req, res) {
+    Product.findOne(
+      { '_id': req.param('productId') },
+      function (err, product) {
+        if (err) {
+          console.log("GET /products/:productId error: " + err);
+          return res.status(500).send('Server error');
+        }
 
-    Product.find(function (err, prods) {
-      if (err) {
-        console.log(err);
+        if (product) {
+          res.json({
+            success: true,
+            product: product
+          });
+        } else {
+          res.status(404).send('Product not found');
+        }
+    });
+  });
+
+  /**
+   * @api {post} /products/ Add product
+   * @apiGroup products
+   *
+   * @apiParam {String} sku  Product SKU.
+   * @apiParam {String} name Product name in Chinese.
+   * @apiParam {String} englishName Product english name.
+   * @apiParam {String} categoryName Product category in Chinese.
+   * @apiParam {String} categoryId Category ID.
+   * @apiParam {String} description Product description, in HTML.
+   * @apiParam {String} instructions Instructions for use.
+   * @apiParam {String} size Product volume.
+   * @apiParam {String} origin Product origin.
+   * @apiParam {String} fullPrice Product's full price.
+   * @apiParam {String} [salePrice] (Optional) Sale price, if exists.
+   * @apiParam {String} [priceCurrency] Price currency; defaults to HKD.
+   *
+   * @apiDescription Adds a new product to the collection.
+   * @apiSuccess {Object} product Returns the created product.
+   */
+  app.post('/', function (req, res) {
+    var product = {
+      sku: req.body.sku,
+      name: req.body.name,
+      englishName: req.body.englishName,
+      categoryName: req.body.categoryName,
+      categoryId: req.body.categoryId,
+      description: req.body.description,
+      instructions: req.body.instructions,
+      size: req.body.size,
+      origin: req.body.origin,
+      fullPrice: req.body.fullPrice,
+      salePrice: req.body.salePrice ? req.body.salePrice : '',
+      priceCurrency: req.body.priceCurrency ? req.body.priceCurrency : 'HKD'
+    };
+
+    // Input checking
+    var missingKeys = [];
+    _.each(product, function(value, key) {
+      if (key !== 'salePrice' && key !== 'priceCurrency' && !value) {
+        missingKeys.push(key);
       }
-
-//       var output = new Array(prods.length);
-      var output = "";
-
-      for (var i = 0; i < prods.length; i++) {
-        var model =
-            {
-              name: prods[i].name,
-              volume: prods[i].volume,
-              prettyVolume: prods[i].prettyVolume(),
-              price: prods[i].price,
-              prettyPrice: prods[i].prettyPrice()
-            };
-        console.log(model);
-//         output[i] = model;
-        output = output + JSON.stringify(model, null, 0);
-      }
-
-      res.json({ message: 'All products: ' + output });
     });
 
-  });
-
-  /**
-     *  Retrieves a specific product for editing.
-     */
-  app.get('/:item_id', function (req, res) {
-
-    Product.findOne({ '_id': req.param('item_id') }, function (err, prod) {
-      if (err) {
-        console.log(err);
-      }
-
-      var model =
-          {
-            name: prod.name,
-            volume: prod.volume,
-            price: prod.price
-          };
-
-      res.json({ message: 'Searched product: ' + JSON.stringify(model, null, 1) });
-    });
-
-  });
-
-
-  /**
-     * Add a new product to the database.
-     */
-  app.post('/add', function (req, res) {
-    var name = req.body.name && req.body.name.trim();
-
-    var volume = parseInt(req.body.volume);
-
-//     var price = parseFloat(req.body.price, 10);
-//    var price = bignum(req.body.price, 10);
-      var price = bigdecimal(req.body.price, 10);
-
-    //Some very lightweight input checking
-    if (name === '' || isNaN(price) || isNaN(volume)) {
-      res.redirect('/products#BadInput');
-      return;
+    if (missingKeys.length > 0) {
+      return res.status(400).send('Params missing: ' + missingKeys);
     }
 
-    var newProduct = new Product({name: name, volume: volume, price: price});
-
-    //Show it in console for educational purposes...
-    newProduct.whatAmI();
-
-    /* The call back recieves to more arguments ->product/s that is/are added to the database
-	   and number of rows that are affected because of save, which right now are ignored
-	   only errors object is consumed*/
-    newProduct.save(function(err) {
+    var newProduct = new Product(product);
+    newProduct.save(function(err, savedProduct) {
       if(err) {
-        console.log('save error', err);
+        console.log("POST /products/ error: " + err);
+        return res.status(500).send('Server error');
       }
 
-      res.redirect('/products');
+      res.json({
+        success: true,
+        product: savedProduct
+      });
     });
   });
 
   /**
-     * Delete a product.
-     * @param: req.body.item_id Is the unique id of the product to remove.
-     */
-  app.delete('/one', function (req, res) {
-    Product.remove({_id: req.body.item_id}, function (err) {
-      if (err) {
-        console.log('Remove error: ', err);
+   * @api {delete} /products/:productId Delete product
+   * @apiGroup products
+   *
+   * @apiParam {String} productId  Product ID to delete.
+   *
+   * @apiDescription Deletes a product.
+   * @apiSuccess {Object} product Product details.
+   */
+  app.delete('/:productId', function (req, res) {
+    Product.remove({_id: req.param('productId')}, function (err, response) {
+      if(err) {
+        console.log("DELETE /products/ error: " + err);
+        return res.status(500).send('Server error');
       }
-      res.redirect('/products');
+
+      res.json({
+        success: true,
+        results: response.result
+      });
     });
   });
 
-
-  /**
-     * Edit a product.
-     * Not implemented here
-     */
-  app.put('/products', function (req, res) {
-    console.log('PUT received. Ignoring.');
-    res.redirect('/products');
-  });
-
+  // TODO
+  //
+  // GET /products/categories => list of categoryIDs and names
+  // GET /products/categories/:category_id
 };
