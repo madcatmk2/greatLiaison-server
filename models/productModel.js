3c0211e22c7de4da437f80aa03f30551bfc5be45
Herman Ng <heymanhn@gmail.com>
2016-05-10 22:10:04 -0700

    Create products JSON file and update schema
    A simple mongoimport run can import these products into the mongo
    collection. The Products schema has also been updated to match the
    schema in the JSON file. [Finishes #119339075]

--- models/productModel.js
+++ models/productModel.js
@@ -3,35 +3,21 @@
 var mongoose = require('mongoose');
 
 var productModel = function () {
-
-  //Define a super simple schema for our products.
   var productSchema = mongoose.Schema({
+    sku: String,
     name: String,
-    volume: Number,
-    price: Number,
-    stock: Number
+    englishName: String,
+    category: String,
+    description: String,
+    instructions: String,
+    size: String,
+    origin: String,
+    fullPrice: String,
+    salePrice: String,
+    priceCurrency: String
   });
 
-  //Verbose toString method
-  productSchema.methods.whatAmI = function () {
-    var greeting = this.name ?
-        'Hello, I\'m a ' + this.name + ' with volume of ' + this.volume + 'mL. I\'m worth $' + this.price
-    : 'I don\'t have a name :(';
-    console.log(greeting);
-  };
-
-  //Format the volume of the product to show unit of measurement
-  productSchema.methods.prettyVolume = function () {
-    return this.volume + ' mL';
-  };
-
-  //Format the price of the product to show a dollar sign, and two decimal places
-  productSchema.methods.prettyPrice = function () {
-    return '$' + this.price.toFixed(2);
-  };
-
   return mongoose.model('Product', productSchema);
-
 };
 
 module.exports = new productModel();
